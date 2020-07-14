8b01ec97d1849815866c1f330ceb8e6c6301b4a3
Brenda Miranda <brendamndx@gmail.com>
2020-07-14 15:35:09 -0300

    Create a new conditional statement (if, else)

--- #1 - Fundamentos/Aula 06 - Comandos de bloco/js/script.js
+++ #1 - Fundamentos/Aula 06 - Comandos de bloco/js/script.js
@@ -9,3 +9,18 @@
  * 9 === '9'
  * result: false
  */
+
+
+// Estrutura de decisão (if, else)
+var a = 8;
+var b = 8;
+
+if ( a > b ) {
+   console.log(a + ' é maior que ' + b);
+} else { //não posso afirmar que a é menor que b automaticamente, pois eles podem ser iguais, logo, outro if será necessário.
+   if ( a < b ) {
+      console.log(a + ' é menor que ' + b);
+   } else {
+      console.log(a + ' é igual a ' + b);
+   }
+}
