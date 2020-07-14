86be7cfcbeeea895687dc91ca2a7eac6e5d08a59
Brenda Miranda <brendamndx@gmail.com>
2020-07-14 14:25:22 -0300

    Add comments in js examples

--- #1 - Fundamentos/Aula 5 - Introdução ao Javascript/js/script.js
+++ #1 - Fundamentos/Aula 5 - Introdução ao Javascript/js/script.js
@@ -2,3 +2,10 @@ console.log('Hello, World');
 
 var title = document.querySelector('h1');
 title.textContent = 'Modificado por Raphael Gomide';
+
+// Comentário de apenas uma linha
+
+/**
+ * Comentário de bloco com múltiplas linhas
+ * Exemplo
+ */
