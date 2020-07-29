0088aaa78a7f9378f305b70abce3200c4ca164ad
Brenda Miranda <brendamndx@gmail.com>
2020-07-29 16:35:30 -0300

    Add examples of import and export modules in ESmodules

---
+++ 2-desenvolvimento-de-apis/aula5-modules-do-nodejs/ESmodules/index.js
@@ -0,0 +1,3 @@
+import soma from './math.js';
+
+console.log(soma(5, 5));
---
+++ 2-desenvolvimento-de-apis/aula5-modules-do-nodejs/ESmodules/math.js
@@ -0,0 +1,9 @@
+function soma(a, b) {
+   return a + b;
+}
+
+function subtracao(a, b) {
+   return a - b;
+}
+
+export default soma;
