b5b8d7c4c8fe7503bd23f00129a64f6a940f06b7
Brenda Miranda <brendamndx@gmail.com>
2020-07-29 16:35:12 -0300

    Add examples of import and export modules in commonJS

---
+++ 2-desenvolvimento-de-apis/aula5-modules-do-nodejs/commonJS/index.js
@@ -0,0 +1,4 @@
+const math = require('./math.js');
+
+console.log(math.soma(2, 3));
+console.log(math.subtracao(5, 2));
---
+++ 2-desenvolvimento-de-apis/aula5-modules-do-nodejs/commonJS/math.js
@@ -0,0 +1,9 @@
+function soma(a, b) {
+   return a + b;
+}
+
+function subtracao(a, b) {
+   return a - b;
+}
+
+module.exports = {soma, subtracao};
